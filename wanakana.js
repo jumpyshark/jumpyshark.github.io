6a8ed9553f02827fd7f6f4ddb2f52dee92cda0bd
Jumpy Shark <sthaabi1@gmail.com>
2022-06-18 00:35:22 +0545

    Auto focucs

--- wanakana.js
+++ wanakana.js
@@ -8,17 +8,6 @@ window.onload = function() {
   document.getElementById("answer").focus();
 }
 
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("btn02").click();
-  }
-}
-
 function genWord(){
     word = "";  
     wordLen = Math.floor(Math.random() * (8-0))+1;
