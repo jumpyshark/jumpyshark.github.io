85dba13f9ef496800df18cebad79af9bbe0331ba
GreisYoung <sthaabi1@gmail.com>
2022-07-06 11:25:03 +0545

    vocabulary update

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function openmenu(){
+if(window.innerWidth<600) document.getElementById("nav-btn").click();
+}
+
+function intro(){
+    if(window.innerWidth>600) document.getElementById("intro").innerHTML = "Please browse the available contents using menu above. :)";
+
+}
